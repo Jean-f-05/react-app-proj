c2270d99d317354a04b0295e25a9e68367d8722c
John Francis <jean_f_05@hotmail.com>
2022-06-09 16:05:55 +0100

    added Expenses styles

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import "./ExpenseDate.css";
+
 function ExpenseDate(data) {
   const month = data.date.toLocaleString("en-US", { month: "long" });
   const year = data.date.toLocaleString("en-US", { year: "numeric" });
   const day = data.date.toLocaleString("en-US", { day: "2-digit" });
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
