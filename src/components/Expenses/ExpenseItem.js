7e5b8a2e9f9503d88c7f783858ce6c68c9da581e
John Francis <jean_f_05@hotmail.com>
2022-06-11 11:18:05 +0100

    refactored card component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,16 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
 function ExpenseItem(data) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={data.date} />
       <div className="expense-item__description">
         <h2>{data.title}</h2>
         <div className="expense-item__price">{data.amount} €</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
