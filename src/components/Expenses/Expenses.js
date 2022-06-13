6d46e254506df933fefa3ec8da35228588641362
John Francis <jean_f_05@hotmail.com>
2022-06-13 14:46:42 +0100

    added stateful list expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,13 @@ function Expenses(data) {
     console.log(year);
   };
 
+  const expenseYears = data.items.filter(
+    (expense) =>
+      expense.date.toLocaleString("en-US", { year: "numeric" }) === year
+  );
+
+  console.log(data);
+
   return (
     <div>
       <Card className="expenses">
@@ -19,26 +26,14 @@ function Expenses(data) {
           selected={year}
           onFilterExpenseChange={saveFilterDatehandler}
         />
-        <ExpenseItem
-          title={data.items[0].title}
-          amount={data.items[0].amount}
-          date={data.items[0].date}
-        />
-        <ExpenseItem
-          title={data.items[1].title}
-          amount={data.items[1].amount}
-          date={data.items[1].date}
-        />
-        <ExpenseItem
-          title={data.items[2].title}
-          amount={data.items[2].amount}
-          date={data.items[2].date}
-        />
-        <ExpenseItem
-          title={data.items[3].title}
-          amount={data.items[3].amount}
-          date={data.items[3].date}
-        />
+        {expenseYears.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
